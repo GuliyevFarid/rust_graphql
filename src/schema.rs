21b280526729f10d49980c2fb040e27450d449d4
Farid Guliyev <guliyev.farid@gmail.com>
2025-02-22 23:38:44 +0100

    Day 1 - Homework

--- src/schema.rs
+++ src/schema.rs
@@ -4,6 +4,7 @@ use async_graphql::{SimpleObject, Object, EmptyMutation, EmptySubscription, Sche
 pub struct User {
     pub id: i32,
     pub name: String,
+    pub email: String,
 }
 
 pub struct QueryRoot;
@@ -14,6 +15,24 @@ impl QueryRoot {
         User {
             id: 1,
             name: "Farid".to_string(),
+            email: "farid@example.com".to_string(),
+        }
+    }
+
+    async fn user_by_id(&self, id: i32) -> User {
+        if id == 1 {
+            User {
+                id: 1,
+                name: "Farid".to_string(),
+                email: "farid@example.com".to_string(),
+            }
+        }
+        else {
+            User {
+                id: 0,
+                name: "".to_string(),
+                email: "".to_string()
+            }
         }
     }
 }
